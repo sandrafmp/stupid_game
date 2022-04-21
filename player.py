45af774bd327402d762f09d931bf1723ff303fc1
Lia Poidomani <lia@Lias-MacBook-Pro.local>
2022-04-21 22:23:49 +0200

    first changes

--- player.py
+++ player.py
@@ -3,6 +3,46 @@ import traceback
 import pygame
 import sys, os
 
+class Display():
+    def __init__(self, game):
+        pygame.init()
+
+    def analyze_events(self, side):
+        events = []
+        for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+
+			if event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+
+				mouseX = event.pos[0] 
+				mouseY = event.pos[1] 
+
+				clicked_row = int(mouseY // SQUARE_SIZE)
+				clicked_col = int(mouseX // SQUARE_SIZE)
+
+				if available_square( clicked_row, clicked_col ):
+
+					mark_square( clicked_row, clicked_col, player )
+					if check_win( player ):
+						game_over = True
+					player = player % 2 + 1
+
+					draw_figures()
+					
+		return events
+					
+
+    def refresh(self):
+
+    def tick(self):
+        self.clock.tick(FPS)
+
+    @staticmethod
+    def quit():
+        pygame.quit()
+
+
 def main(ip_address, port):
     try:
         with Client((ip_address, port), authkey=b'secret password') as conn:
