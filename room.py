d1b76b7dcedadbbf5a758421d41951f228492ea4
Sandra Fjelkestam <sandrafj@kth.se>
2022-04-21 11:53:59 +0200

    room 2

---
+++ room.py
@@ -0,0 +1,39 @@
+from multiprocessing import Process, Manager, Value, Lock
+from multiprocessing.connection import Listener
+import traceback
+import sys
+
+
+def main(ip_address, port):
+    manager = Manager()
+    try:
+        with Listener((ip_address, port),
+                      authkey=b'secret password') as listener:
+            n_player = 0
+            players = [None, None]
+            game = Game(manager)
+            while True:
+                print(f"accepting connection {n_player}")
+                conn = listener.accept()
+                players[n_player] = Process(target=player,
+                                            args=(n_player, conn, game))
+                n_player += 1
+                if n_player == 2:
+                    players[0].start()
+                    players[1].start()
+                    n_player = 0
+                    players = [None, None]
+                    game = Game(manager)
+
+    except Exception as e:
+        traceback.print_exc()
+
+if __name__=='__main__':
+    port = 24654
+    ip_address = "127.0.0.1"
+    if len(sys.argv)>1:
+        ip_address = sys.argv[1]
+    if len(sys.argv) > 2:
+        port = int(sys.argv[2])
+
+    main(ip_address, port)
