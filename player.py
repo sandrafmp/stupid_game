71ac9cb9edcb1aa3d420fb3ccdaef936b17d1e2d
Sandra Fjelkestam <sandrafj@kth.se>
2022-04-21 12:53:54 +0200

    player test

--- player.py
+++ player.py
@@ -1,1 +1,39 @@
-#empty file
+from multiprocessing.connection import Client
+import traceback
+import pygame
+import sys, os
+
+def main(ip_address, port):
+    try:
+        with Client((ip_address, port), authkey=b'secret password') as conn:
+            game = Game()
+            side,gameinfo = conn.recv()
+            print(f"I am playing {SIDESSTR[side]}")
+            game.update(gameinfo)
+            display = Display(game)
+            while game.is_running():
+                events = display.analyze_events(side)
+                for ev in events:
+                    conn.send(ev)
+                    if ev == 'quit':
+                        game.stop()
+                conn.send("next")
+                gameinfo = conn.recv()
+                game.update(gameinfo)
+                display.refresh()
+                display.tick()
+    except:
+        traceback.print_exc()
+    finally:
+        pygame.quit()
+
+
+if __name__=="__main__":
+    port = 24654
+    ip_address = "127.0.0.1"
+    if len(sys.argv)>1:
+        ip_address = sys.argv[1]
+    if len(sys.argv) > 2:
+        port = int(sys.argv[2])
+
+    main(ip_address, port)
